546d2845e3a439f36b094c43248c439aee9d9a72
Marina Ruiz <marinaruizrodriguez01@gmail.com>
2023-04-11 15:03:16 +0200

    get tests and games

--- main.py
+++ main.py
@@ -48,12 +48,109 @@ def get_points(id_user):
 
     return points
 
+
 @app.route('/users/<id_user>/kidmed', methods=['GET'])
 def get_kidmed(id_user):
     user = json_util.loads(func_get_user(id_user))
     kidmed = user['kidmed']
 
     return kidmed
+
+
+@app.route('/users/<id_user>/totalkidmed', methods=['GET'])
+def get_totalkidmed(id_user):
+    user = json_util.loads(func_get_user(id_user))
+    totalkidmed = user['total_kidmed']
+
+    return totalkidmed
+
+
+@app.route('/users/<id_user>/demtest', methods=['GET'])
+def get_demtest(id_user):
+    user = json_util.loads(func_get_user(id_user))
+    demtest = user['dem_test']
+
+    return demtest
+
+
+@app.route('/users/<id_user>/paqc', methods=['GET'])
+def get_paqc(id_user):
+    user = json_util.loads(func_get_user(id_user))
+    paqc = user['paqc']
+
+    return paqc
+
+
+@app.route('/users/<id_user>/pedsql', methods=['GET'])
+def get_pedsql(id_user):
+    user = json_util.loads(func_get_user(id_user))
+    pedsql = user['pedsql']
+
+    return pedsql
+
+
+@app.route('/users/<id_user>/initialtest', methods=['GET'])
+def get_initialtest(id_user):
+    user = json_util.loads(func_get_user(id_user))
+    initialtest = user['initial_test']
+
+    return initialtest
+
+
+@app.route('/users/<id_user>/game1part1', methods=['GET'])
+def get_game1part1(id_user):
+    user = json_util.loads(func_get_user(id_user))
+    game1part1 = user['game1_part1']
+
+    return game1part1
+
+
+@app.route('/users/<id_user>/game1part2', methods=['GET'])
+def get_game1part2(id_user):
+    user = json_util.loads(func_get_user(id_user))
+    game1part2 = user['game1_part2']
+
+    return game1part2
+
+
+@app.route('/users/<id_user>/game2', methods=['GET'])
+def get_game2(id_user):
+    user = json_util.loads(func_get_user(id_user))
+    game2 = user['game2']
+
+    return game2
+
+
+@app.route('/users/<id_user>/game3part1', methods=['GET'])
+def get_game3part1(id_user):
+    user = json_util.loads(func_get_user(id_user))
+    game3part1 = user['game3_part1']
+
+    return game3part1
+
+
+@app.route('/users/<id_user>/game3part2', methods=['GET'])
+def get_game3part2(id_user):
+    user = json_util.loads(func_get_user(id_user))
+    game3part2 = user['game3_part2']
+
+    return game3part2
+
+
+@app.route('/users/<id_user>/game4', methods=['GET'])
+def get_game4(id_user):
+    user = json_util.loads(func_get_user(id_user))
+    game4 = user['game4']
+
+    return game4
+
+
+@app.route('/users/<id_user>/survey', methods=['GET'])
+def get_game1(id_user):
+    user = json_util.loads(func_get_user(id_user))
+    survey = user['survey']
+
+    return survey
 
 
 # CONVERSATION
