40673e2e0e0638236b21e887033c43c11fd3a651
Marina Ruiz <marinaruizrodriguez01@gmail.com>
2023-04-19 12:42:15 +0200

    test export

--- main.py
+++ main.py
@@ -295,7 +295,8 @@ def export_xlsx():
     user_documents = mongo.db.user.find()
     for doc in user_documents:
         user = json_util.loads(json_util.dumps(doc))
-        demtest_age.append(int(user['dem_test']['age']))
+        demtest = json_util.loads(json_util.dumps(user['dem_test']))
+        demtest_age.append(demtest['age'])
     df = pd.DataFrame(data=demtest_age)
     print (df)
 
