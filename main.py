0cba6a3970001e795ab880358d249cffe8c61a27
Marina Ruiz <marinaruizrodriguez01@gmail.com>
2023-04-26 14:31:44 +0200

    remove file before write

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ from flask_cors import CORS
 from flask import Flask, jsonify, request
 from flask_pymongo import PyMongo
 from bson import json_util, ObjectId
-import numpy as np
 import pandas as pd
 import data
+import os
 
 app = Flask(__name__)
 app.config['MONGO_URI'] = "mongodb://localhost:27017/bon-app-petit"
@@ -317,6 +317,8 @@ def export_xlsx():
     game3 = data.game3()
     game4 = data.game4()
     survey = data.survey()
+
+    os.remove("data_bonappetit.xlsx")
 
     with pd.ExcelWriter('data_bonappetit.xlsx') as writer:
         demtest.to_excel(writer, sheet_name='Test demográfico')
